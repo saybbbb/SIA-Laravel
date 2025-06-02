93bb7764d33960e40ea856b3f5cdca9b50e50a87
Storm <kjialopier@gmail.com>
2025-06-02 21:47:09 +0800

    Updated seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call([
+            SupplierSeeder::class,
+            WaterSeeder::class,
+        ]);
 
         User::factory()->create([
             'name' => 'Test User',
